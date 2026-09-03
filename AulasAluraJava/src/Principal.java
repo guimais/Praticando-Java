7748c4156f153fcb69e8f55c14fd19dc1d90afe7
Guilherme Mais <seu-email-do-github@example.com>
2026-09-02 23:05:05 -0300

    Ultimas modificacoes

---
+++ AulasAluraJava/src/Principal.java
@@ -0,0 +1,39 @@
+import br.com.alura.AulasAluraJava.calculos.CalculadoraDeTempo;
+import br.com.alura.AulasAluraJava.modelos.Filme;
+import br.com.alura.AulasAluraJava.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefao");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duracao do filme: "+ meuFilme.getDuracaoEmMinutos());
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total de avaliacoes: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+        // meuFilme.somaDasAvaliacoes = 10;
+        // meuFilme.totalDeAvaliacoes = 1;
+        //System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEspisodio(50);
+        System.out.println("Duracao para maratonar lost: "+ lost.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        System.out.println(calculadora.getTempoTotal());
+
+    }
+}
